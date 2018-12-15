86fccf2c990041ae1d95007971827460011c8577
Will Hickman <whickman@epic.com>
2018-12-15 12:04:45 -0600

    add comments to Constants class

--- GroupProject/application/Constants.java
+++ GroupProject/application/Constants.java
@@ -1,25 +1,44 @@
+/**
+ * Filename:   Constants.java
+ * Project:    Group Project
+ * Authors:    sapan (sapan@cs.wisc.edu), Soua Lor, Maria Helgeson, Daniel Walter, & Will Hickman
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400 - Lecutre 46373
+ * 
+ * Due Date:   12/16/18
+ * Version:    1.0
+ * 
+ * Credits:    N/A
+ * 
+ * Bugs:       No known bugs
+ */
 package application;
 
 /**
- * 
- * @author whickman
- * Resources
- * - constant list implementation from here - https://stackoverflow.com/questions/9285011/declaring-an-arraylist-object-as-final-for-use-in-a-constants-file
+ * Set of constants used throughout the program to centralize and standardize data. 
  */
 public class Constants {
 	
+	// hex ID length used for food items
 	public static final int IdLength = 24;
 	
+	// current working directory of the program
 	public static final String CurrentDirectory = System.getProperty("user.dir");
 	
+	// initial path to a food items list - used at startup
 	public static final String InitialDataPath = CurrentDirectory + "\\foodItems.csv";
 	
+	// default path to save food options list to
 	public static final String DefaultSavePath = CurrentDirectory + "\\mySave.csv";
 	
+	// list of comparators used for filtering food based on nutrient values
 	public static final String[] Comparators = new String[] { "<=", "==", ">=" };
 	
+	// hex digits - used for generating hex IDs for user-added food items
 	public static final char[] HexDigits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };	
 	
+	// list of possible io message types used for standardizing load and save responses
 	public enum IOMessage
 	{
 		Success,
@@ -29,6 +48,7 @@ public class Constants {
 		UnexpectedEx
 	}
 	
+	// possible nutrient labels from serialized data on file
 	public enum Nutrient
 	{
 		calories,
